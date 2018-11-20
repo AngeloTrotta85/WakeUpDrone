48298d5039d9c9f38928dc23749d102d2f979f54
AngeloTrotta85 <angelo@magilia.com>
2018-11-20 15:57:23 +0100

    add coord

--- src/WakeUpDrone.cpp
+++ src/WakeUpDrone.cpp
@@ -6,10 +6,28 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-#include <iostream>
-using namespace std;
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>     // std::cout
+#include <fstream>      // std::ifstream
+#include <algorithm>    // std::find
+#include <vector>       // std::vector
+#include <list>       // std::list
+#include <cstdlib>
+#include <ctime>
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+#include <boost/range/irange.hpp>
+#include <boost/math/special_functions/factorials.hpp>
+#include "MyCoord.h"
+
+using namespace std;
+using namespace boost;
+
+int main(int argc, char **argv) {
+	std::list<MyCoord> pointsList;
+
+	cout << "Wake-up Drone BEGIN!!!" << endl;
+
+	cout << "Wake-up Drone FINISH!!!" << endl;
+	return EXIT_SUCCESS;
 }
