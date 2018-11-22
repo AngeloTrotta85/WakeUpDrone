776065f4682b2525b6683ab81075050b9fc5b7b8
AngeloTrotta85 <angelo@magilia.com>
2018-11-22 19:21:49 +0100

    ok

--- src/WakeUpDrone.cpp
+++ src/WakeUpDrone.cpp
@@ -15,10 +15,17 @@
 #include <list>       // std::list
 #include <cstdlib>
 #include <ctime>
+#include <random>
+#include <chrono>
 
 #include <boost/range/irange.hpp>
 #include <boost/math/special_functions/factorials.hpp>
 #include "MyCoord.h"
+
+#define K_D 50
+#define K_T 30
+#define K_E 20
+#define K_L 20
 
 using namespace std;
 using namespace boost;
@@ -35,6 +42,8 @@ const char* COLOR_LIST_10[] = {
 		"salmon",
 		"greenyellow"
 };
+
+std::default_random_engine *generator_rand;
 
 class InputParser{
 public:
@@ -59,12 +68,54 @@ private:
 	std::vector <std::string> tokens;
 };
 
+class Sensor;
+class UAV;
+
+class Readings {
+public:
+	Readings(Sensor *s, UAV *u, int timestamp, double val) {
+		sensor = s;
+		uav = u;
+		value = val;
+		read_time = timestamp;
+	}
+
+public:
+	Sensor *sensor;
+	UAV *uav;
+	int  read_time;
+	double value;
+};
+
+class Sensor {
+public:
+	Sensor(MyCoord sensCoord, double re) {
+		coord = sensCoord;
+		residual_energy = re;
+	}
+
+public:
+	MyCoord coord;
+	double residual_energy;
+	std::list<Readings *> mySensorReadings;
+};
+
+class UAV {
+public:
+	UAV(MyCoord recCoord) {
+		recharge_coord = recCoord;
+	}
+
+public:
+	MyCoord recharge_coord;
+	std::list<Readings *> mySensorReadings;
+};
 
 class CoordCluster{
 public:
-	CoordCluster (MyCoord *uav, int cID){
+	CoordCluster (UAV *uav, int cID){
 		clusterID = cID;
-		clusterHead = new MyCoord(uav->x, uav->y);
+		clusterHead = new MyCoord(uav->recharge_coord.x, uav->recharge_coord.y);
 		clusterUAV = uav;
 	}
 
@@ -76,8 +127,13 @@ public:
 		pointsList.clear();
 	}
 
-	bool checkChange(void) {
-		bool ris = false;
+	bool checkNotChange(void) {
+		/*cout << "Checking ";
+		for (auto& pp : pointsList) cout << pp << " ";
+		cout << " VS ";
+		for (auto& pp : pointsList_bkp) cout << pp << " ";
+		cout << endl;*/
+		bool ris = true;
 		for (auto& pp : pointsList) {
 			bool found = false;
 			for (auto& pp_b : pointsList_bkp) {
@@ -87,59 +143,74 @@ public:
 				}
 			}
 			if (found == false) {
-				ris = true;
+				ris = false;
 				break;
 			}
 		}
 		return (ris && (pointsList.size() == pointsList_bkp.size()));
 	}
 
-	void printCluter(void) {
-		cout << "UAV " << *clusterUAV << " - ";
-		for (auto it1 = pointsList.begin(); it1 != pointsList.end(); it1++){
-			bool ch = (*it1 == clusterHead);
-			if (ch) {
-				cout << "*";
-			}
-			cout << *(*it1);
-			if (ch) {
-				cout << "*";
-			}
-			cout << " ";
-		}
-	}
-
 public:
 	//std::list<MyCoord> pointsList;
-	std::list<MyCoord *> pointsList;
-	std::list<MyCoord *> pointsList_bkp;
+	std::list<Sensor *> pointsList;
+	std::list<Sensor *> pointsList_bkp;
 	MyCoord *clusterHead;
-	MyCoord *clusterUAV;
+	UAV *clusterUAV;
 	int clusterID;
 };
 
-
-double fRand(double fMin, double fMax)
-{
-    double f = (double)rand() / (double)RAND_MAX;
-    return fMin + f * (fMax - fMin);
+double algebraic_sum(double a, double b) {
+	return (a + b - (a * b));
 }
 
-void generateRandomSensors(std::list<MyCoord *> &pl, int ss, int ns) {
+void generateRandomSensors(std::list<Sensor *> &pl, int ss, int ns) {
+
 	for (int i : boost::irange(0, ns)) { // i goes from 0 to ns-1
-		MyCoord *newCoord = new MyCoord(fRand(0, ss), fRand(0, ss));
-		pl.push_back(newCoord);
-		cout << "Sensor: " << i << " --> " << newCoord << endl;
-		//cout << newCoord.x << ";" << newCoord.y << endl;
+		std::uniform_real_distribution<double> uniform_distribution(0.0, ss);
+		std::normal_distribution<double> n_distribution(10000.0,1000.0);
+
+		Sensor *newS = new Sensor(MyCoord(uniform_distribution(*generator_rand), uniform_distribution(*generator_rand)), n_distribution(*generator_rand));
+		pl.push_back(newS);
+		cout << "Sensor: " << i << " --> " << newS->coord << endl;
 	}
 }
 
-void generateRandomUAVs(std::list<MyCoord *> &pl, int ss, int nu) {
+void generateRandomUAVs(std::list<UAV *> &pl, int ss, int nu) {
 	for (int i : boost::irange(0, nu)) { // i goes from 0 to nu-1
-		MyCoord *newCoord = new MyCoord(fRand(0, ss), fRand(0, ss));
-		pl.push_back(newCoord);
-		cout << "UAV: " << i << " --> " << newCoord << endl;
-		//cout << newCoord.x << ";" << newCoord.y << endl;
+		std::uniform_real_distribution<double> uniform_distribution(0.0, ss);
+		UAV *newU = new UAV(MyCoord(uniform_distribution(*generator_rand), uniform_distribution(*generator_rand)));
+		pl.push_back(newU);
+		cout << "UAV: " << i << " --> " << newU->recharge_coord << endl;
+	}
+}
+
+void importSensorsFromFile(std::string inputFileName, std::list<Sensor *> &pl) {
+	std::ifstream fileInput(inputFileName, std::ifstream::in);
+	std::string str;
+	double x, y, e;
+	if(fileInput.is_open()) {
+		while (std::getline(fileInput, str)) {
+			sscanf(str.c_str(), "%lf;%lf;%lf", &x, &y, &e);
+			Sensor *np = new Sensor(MyCoord(x, y), e);
+			pl.push_back(np);
+			//cout << "From file: " << str << ". Parsed sensor: " << x << ";" << y << endl;
+		}
+		fileInput.close();
+	}
+}
+
+void importUAVsFromFile(std::string inputFileName, std::list<UAV *> &pl) {
+	std::ifstream fileInput(inputFileName, std::ifstream::in);
+	std::string str;
+	double x, y;
+	if(fileInput.is_open()) {
+		while (std::getline(fileInput, str)) {
+			sscanf(str.c_str(), "%lf;%lf", &x, &y);
+			UAV *np = new UAV(MyCoord(x, y));
+			pl.push_back(np);
+			//cout << "From file: " << str << ". Parsed sensor: " << x << ";" << y << endl;
+		}
+		fileInput.close();
 	}
 }
 
@@ -153,7 +224,6 @@ void importPoints(std::string inputFileName, std::list<MyCoord *> &pl) {
 			sscanf(str.c_str(), "%lf;%lf", &x, &y);
 			MyCoord *np = new MyCoord(x, y);
 			pl.push_back(np);
-
 			//cout << "From file: " << str << ". Parsed: " << x << ";" << y << endl;
 		}
 
@@ -162,19 +232,35 @@ void importPoints(std::string inputFileName, std::list<MyCoord *> &pl) {
 }
 
 
+void writeOnFileSensors(std::string fn, std::list<Sensor *> pointList) {
+	std::ofstream fout(fn, std::ofstream::out);
+	if (fout.is_open()) {
+		for (auto& p : pointList) { // i goes from 0 to (ni-1) inclusive
+			fout << p->coord.x << ";" << p->coord.y << ";" << p->residual_energy << endl;
+		}
+		fout.close();
+	}
+}
+void writeOnFileUAVs(std::string fn, std::list<UAV *> pointList) {
+	std::ofstream fout(fn, std::ofstream::out);
+	if (fout.is_open()) {
+		for (auto& p : pointList) { // i goes from 0 to (ni-1) inclusive
+			fout << p->recharge_coord.x << ";" << p->recharge_coord.y << endl;
+		}
+		fout.close();
+	}
+}
 void writeOnFilePoints(std::string fn, std::list<MyCoord *> pointList) {
 	std::ofstream fout(fn, std::ofstream::out);
-
 	if (fout.is_open()) {
 		for (auto& p : pointList) { // i goes from 0 to (ni-1) inclusive
 			fout << p->x << ";" << p->y << endl;
 		}
 		fout.close();
 	}
-
 }
 
-void generateDOTfile(std::string outFileName, std::vector<CoordCluster *> &clustVec, double pSize){
+void generateDOTfile(std::string outFileName, std::vector<CoordCluster *> &clustVec, double sSize, double pSize){
 	std::ofstream fout(outFileName, std::ofstream::out);
 	int count = 1;
 	int count_uav = 1;
@@ -203,12 +289,19 @@ void generateDOTfile(std::string outFileName, std::vector<CoordCluster *> &clust
 
 		fout << "graph G{" << endl;
 
+		fout 	<< "{node [style=invis] P00 P01 P10 P11}"
+				<< "P00 [pos = \"0,0!\"]"
+				<< "P01 [pos = \"0," << sSize << "!\"]"
+				<< "P10 [pos = \"" << sSize << ",0!\"]"
+				<< "P11 [pos = \"" << sSize << "," << sSize << "!\"]"
+				<< endl << endl;
+
 		for (int i = 0; i < (int) clustVec.size(); i++) {
 			std::string color = std::string(COLOR_LIST_10[i%10]);
 
 			for (auto& p : clustVec[i]->pointsList) {
 				fout << "S" << count << " [shape=\"point\" color=\"" << color << "\" pos=\""
-						<< p->x << "," << p->y << "!\" width=" << pSize << ", height=" << pSize << "]" << endl;
+						<< p->coord.x << "," << p->coord.y << "!\" width=" << pSize << ", height=" << pSize << "]" << endl;
 
 				/*if (i == maxIdx) {
 					fout << "S" << count << "_rad [shape=\"circle\" color=\"" << "black" << "\" style=\"dotted\" label=\"\" pos=\""
@@ -219,7 +312,7 @@ void generateDOTfile(std::string outFileName, std::vector<CoordCluster *> &clust
 			}
 
 			fout << "U" << count_uav << " [shape=\"point\" color=\"" << color << "\" pos=\""
-					<< clustVec[i]->clusterUAV->x << "," << clustVec[i]->clusterUAV->y << "!\" width=" << pSize*3 << ", height=" << pSize*3 << "]" << endl;
+					<< clustVec[i]->clusterUAV->recharge_coord.x << "," << clustVec[i]->clusterUAV->recharge_coord.y << "!\" width=" << pSize*3 << ", height=" << pSize*3 << "]" << endl;
 
 			fout << "C" << count_uav << " [shape=\"point\" color=\"" << color << "\" pos=\""
 					<< clustVec[i]->clusterHead->x << "," << clustVec[i]->clusterHead->y << "!\" width=" << pSize*2 << ", height=" << pSize*2 << "]" << endl;
@@ -233,11 +326,66 @@ void generateDOTfile(std::string outFileName, std::vector<CoordCluster *> &clust
 	}
 }
 
-void kmeans_clustering(std::vector<CoordCluster *> &cv, std::list<MyCoord *> &sl) {
+double calculate_loss_energy(Sensor *se, int tk, std::list<Sensor *> &sl) {
+	double ris = 0;
+	for (auto& ss : sl) {
+		if (ss != se) {
+			double exp_exponent = (se->residual_energy - ss->residual_energy) * K_E;
+			double actLoss = 1.0 / (1.0 + exp(exp_exponent));
+			ris = algebraic_sum(ris, actLoss);
+		}
+	}
+	return ris;
+}
+
+double calculate_loss_last(Sensor *se, int tk, std::list<Sensor *> &sl) {
+	double ris = 0;
+	for (auto& ss : sl) {
+		if (ss != se) {
+			int last_tk = 0;
+			if (ss->mySensorReadings.size()> 0) last_tk = ss->mySensorReadings.front()->read_time;
+			double exp_exponent = (tk - last_tk) * K_L;
+			double actLoss = 1.0 - (1.0 / exp(exp_exponent));
+			ris = algebraic_sum(ris, actLoss);
+		}
+	}
+	return ris;
+}
+
+double calculate_loss_distance(Sensor *s1, Sensor *s2) {
+	double exp_exponent = s1->coord.distance(s2->coord) * K_D;
+	return (1.0 / exp(exp_exponent));
+}
+
+double calculate_loss_time(int t1, int t2) {
+	double exp_exponent = abs(t1 - t2) * K_T;
+	return (1.0 / exp(exp_exponent));
+}
+
+double calculate_loss_correlation(Sensor *se, int tk, std::list<Sensor *> &sl) {
+	double ris = 0;
+	for (auto& ss : sl) {
+		if (ss != se) {
+			for (auto& r : ss->mySensorReadings) {
+				double actLoss = calculate_loss_distance(se, ss) + calculate_loss_time(tk, r->read_time);
+				ris = algebraic_sum(ris, actLoss);
+			}
+		}
+	}
+	return ris;
+}
+
+double calculate_loss_full(Sensor *se, int tk, std::list<Sensor *> &sl, double alpha, double beta, double gamma) {
+	double ris = algebraic_sum(alpha * calculate_loss_energy(se, tk, sl), beta * calculate_loss_last(se, tk, sl));
+	return algebraic_sum(ris, calculate_loss_correlation(se, tk, sl));
+}
+
+void kmeans_clustering(std::vector<CoordCluster *> &cv, std::list<Sensor *> &sl) {
 	bool changed = true;
-	int nIter = 100;
+	int nIter = 200;
 
 	while (changed && (nIter > 0)) {
+	//while (nIter > 0) {
 		// clear the clusters
 		for (auto& cc : cv) {
 			cc->clear();
@@ -249,7 +397,7 @@ void kmeans_clustering(std::vector<CoordCluster *> &cv, std::list<MyCoord *> &sl
 			double minDist = std::numeric_limits<double>::max();
 
 			for (auto& cc : cv) {
-				double ddd = ss->distance(*cc->clusterHead);
+				double ddd = ss->coord.distance(*cc->clusterHead);
 				if (ddd < minDist) {
 					minDist = ddd;
 					closestCL = cc;
@@ -266,7 +414,7 @@ void kmeans_clustering(std::vector<CoordCluster *> &cv, std::list<MyCoord *> &sl
 			MyCoord newClusterCoord = MyCoord(0, 0);
 
 			for (auto& ss : cc->pointsList) {
-				newClusterCoord += *ss;
+				newClusterCoord += ss->coord;
 			}
 
 			if (cc->pointsList.size() > 0) {
@@ -280,18 +428,110 @@ void kmeans_clustering(std::vector<CoordCluster *> &cv, std::list<MyCoord *> &sl
 		// check if updated
 		changed = false;
 		for (auto& cc : cv) {
-			if (cc->checkChange()) {
+			if (!cc->checkNotChange()) {
 				changed = true;
 				break;
 			}
 		}
 		nIter--;
 	}
+	cout << "k-means ended in " << 200 - nIter << " iterations" << endl;
+}
+
+
+void kmeans_clustering_withReadings(std::vector<CoordCluster *> &cv, std::list<Sensor *> &sl, int time_now) {
+	bool changed = true;
+	int nIter = 200;
+
+	while (changed && (nIter > 0)) {
+	//while (nIter > 0) {
+		// clear the clusters
+		for (auto& cc : cv) {
+			cc->clear();
+		}
+
+		// assign the sensors to the clusters
+		for (auto& ss : sl) {
+			CoordCluster *closestCL = nullptr;
+			double minDist = std::numeric_limits<double>::max();
+
+			for (auto& cc : cv) {
+				double multiplier = 0.0;
+				if (cc->pointsList.size() > 0) {
+					//multiplier = (1.0 - calculate_loss_full(ss, time_now, cc->pointsList,  0.25, 0.25, 0.5));
+					multiplier = calculate_loss_full(ss, time_now, cc->pointsList,  0.25, 0.25, 0.5);
+				}
+				double ddd = ss->coord.distance(*cc->clusterHead) * multiplier;
+
+				if (ddd < minDist) {
+					minDist = ddd;
+					closestCL = cc;
+				}
+			}
+
+			if (closestCL != nullptr) {
+				closestCL->pointsList.push_back(ss);
+			}
+		}
+
+		// update the cluster heads
+		for (auto& cc : cv) {
+			MyCoord newClusterCoord = MyCoord(0, 0);
+
+			for (auto& ss : cc->pointsList) {
+				newClusterCoord += ss->coord;
+			}
+
+			if (cc->pointsList.size() > 0) {
+				newClusterCoord /= (double) cc->pointsList.size();
+			}
+
+			cc->clusterHead->x = newClusterCoord.x;
+			cc->clusterHead->y = newClusterCoord.y;
+		}
+
+		// check if updated
+		changed = false;
+		for (auto& cc : cv) {
+			if (!cc->checkNotChange()) {
+				changed = true;
+				break;
+			}
+		}
+		nIter--;
+	}
+	cout << "k-means ended in " << 200 - nIter << " iterations" << endl;
+}
+
+void generate_readings(std::list<Sensor *> &sl, std::list<UAV *> &ul, int maxTime) {
+	std::uniform_real_distribution<double> uniform_distribution(0.0, 1.0);
+	std::uniform_int_distribution<int> uniform_distribution_uav(0.0, ul.size()-1);
+	for (auto& ss : sl) {
+		for (int i = 0 ; i < maxTime; i++) {
+			double r_reading = uniform_distribution(*generator_rand);
+
+			if (r_reading < 0.025) {
+				int r_uav = uniform_distribution_uav(*generator_rand);
+
+				//UAV *uOK = *ul.begin();
+				auto uOK = ul.begin();
+				while (r_uav > 0) {
+					--r_uav;
+					uOK++;
+				}
+				UAV *u_reading = *uOK;
+				Readings *read_new = new Readings(ss, u_reading, i, uniform_distribution(*generator_rand));
+				ss->mySensorReadings.push_front(read_new);
+				u_reading->mySensorReadings.push_front(read_new);
+			}
+
+		}
+	}
 }
 
 int main(int argc, char **argv) {
-	std::list<MyCoord *> sensorsList;
-	std::list<MyCoord *> uavsList;
+	std::list<Sensor *> sensorsList;
+	std::list<UAV *> uavsList;
 	std::vector<CoordCluster *> clustersVec;
 	int scenarioSize = 100;
 	int nSensors = 40;
@@ -313,10 +553,11 @@ int main(int argc, char **argv) {
 
 	if (!seedUser.empty()) {
 		int seedR = atoi(seedUser.c_str());
-		std::srand(seedR);
+		generator_rand = new std::default_random_engine(seedR);
 	}
 	else {
-		std::srand(std::time(0)); // use current time as seed for random generator
+		unsigned seedR = std::chrono::system_clock::now().time_since_epoch().count();
+		generator_rand = new std::default_random_engine(seedR);
 	}
 	if (!scenarioMaxVal.empty()) {
 		scenarioSize = atoi(scenarioMaxVal.c_str());
@@ -331,21 +572,21 @@ int main(int argc, char **argv) {
 	if (inputSensorsFileName.empty()) {
 		generateRandomSensors(sensorsList, scenarioSize, nSensors);
 		if (!outputSensorsFileName.empty()) {
-			writeOnFilePoints(outputSensorsFileName, sensorsList);
+			writeOnFileSensors(outputSensorsFileName, sensorsList);
 		}
 	}
 	else {
-		importPoints(inputSensorsFileName, sensorsList);
+		importSensorsFromFile(inputSensorsFileName, sensorsList);
 	}
 
 	if (inputUAVsFileName.empty()) {
 		generateRandomUAVs(uavsList, scenarioSize, nUAV);
 		if (!outputUAVsFileName.empty()) {
-			writeOnFilePoints(outputUAVsFileName, uavsList);
+			writeOnFileUAVs(outputUAVsFileName, uavsList);
 		}
 	}
 	else {
-		importPoints(inputUAVsFileName, uavsList);
+		importUAVsFromFile(inputUAVsFileName, uavsList);
 	}
 
 	clustersVec.resize(uavsList.size(), nullptr);
@@ -356,10 +597,13 @@ int main(int argc, char **argv) {
 		++idd;
 	}
 
-	kmeans_clustering(clustersVec, sensorsList);
+	//kmeans_clustering(clustersVec, sensorsList);
+
+	generate_readings(sensorsList, uavsList, 100);
+	kmeans_clustering_withReadings(clustersVec, sensorsList, 100);
 
 	if (!dotFileOutput.empty()) {
-		generateDOTfile(dotFileOutput, clustersVec, ((double) scenarioSize)/50.0);
+		generateDOTfile(dotFileOutput, clustersVec, scenarioSize, ((double) scenarioSize)/50.0);
 	}
 
 	cout << "Wake-up Drone FINISH!!!" << endl;
